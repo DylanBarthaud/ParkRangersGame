be59732ae3f609c8ad25a0d0f68267bffbc5e4c4
Glitch-o <budderking123.1@gmail.com>
2025-11-25 14:41:15 +0000

    attempted to add generator

---
+++ MultiplayerBase/Assets/GeneratorFuel.cs
@@ -0,0 +1,33 @@
+using Unity.Netcode;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class GeneratorFuel : NetworkBehaviour, IInteractable
+{
+    private NetworkVariable<bool> isBeingPressed = new NetworkVariable<bool>();
+
+    public bool CanInteract(Interactor interactor)
+    {
+        return !isBeingPressed.Value;
+    }
+
+    public void OnInteract(Interactor interactor)
+    {
+        isBeingPressed.Value = true;
+    }
+
+    public void OnInteractHeld(Interactor interactor, int tick)
+    {
+        EventManager.instance.OnButtonHeld(tick, interactor);
+
+        if (tick == 30)
+        {
+            EventManager.instance.OnButtonPressed();
+        }
+    }
+    public void OnInteractReleased(Interactor interactor, int tick)
+    {
+        EventManager.instance.OnButtonReleased();
+        isBeingPressed.Value = false;
+    }
+}
