62e43935c28c33325a712f2d204eb50589c69d88
PinguNSE <148564778+PeppyThePingu@users.noreply.github.com>
2026-02-09 16:12:57 +0000

    Control Rebinding
    Added some basic control rebinds; still needs revert option to allow reverting to default controls though

--- MultiplayerBase/Assets/_Scripts/SettingsHandler.cs
+++ MultiplayerBase/Assets/_Scripts/SettingsHandler.cs
@@ -3,20 +3,36 @@ using System.IO;
 using UnityEngine.UI;
 using System;
 using TMPro;
+using UnityEngine.InputSystem;
+using System.Collections.Generic;
 
 public class SettingsHandler : MonoBehaviour
 {
     [SerializeField] private GameObject settingsMenu;
+    private GameObject rebindPrompt;
+    private string currentRebind;
+    private List<KeyCode> forbiddenKeys = new List<KeyCode>
+    {
+        // These keys are disallowed from being used for keybinds for various reasons
+        KeyCode.Escape,
+        KeyCode.LeftMeta,
+        KeyCode.LeftWindows,
+        KeyCode.RightMeta,
+        KeyCode.RightWindows
+    };
+
     public ClientSettings settings;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        rebindPrompt = settingsMenu.transform.Find("RebindPanel").gameObject;
+
         // Attempt to load settings from file, if not found then use default settings
         try
         {
             string settingsFile = File.ReadAllText("Settings.data");
-            settings = new ClientSettings(true);
+            settings = JsonUtility.FromJson<ClientSettings>(settingsFile);
         }
         catch (FileNotFoundException)
         {
@@ -27,19 +43,108 @@ public class SettingsHandler : MonoBehaviour
 
     void Update()
     {
-        // Might use for some debug things idk
+        if (currentRebind == null)
+        {
+            // Temp debug things to make sure things are working, only runs when a rebind prompt isn't active
+            if (Input.GetKey(settings.keybinds.debugFlash))
+            {
+               settingsMenu.transform.Find("TempText").gameObject.SetActive(true);
+            }
+            else
+            {
+                settingsMenu.transform.Find("TempText").gameObject.SetActive(false);
+            }
+        }
+    }
+    
+    void OnGUI()
+    {
+        if (currentRebind != null)
+        {
+            Event e = Event.current;
+            if (e.isKey)
+            {
+                if (!forbiddenKeys.Contains(e.keyCode)) // Checks if the key is allowed to be used for keybinds
+                {
+                    SetControl(currentRebind, e.keyCode);
+                    currentRebind = null;
+                    rebindPrompt.SetActive(false);
+                }
+            }
+        }
     }
 
     public void ReadSettings()
     {
         GameObject tempText = settingsMenu.transform.Find("TempText").gameObject;
         tempText.GetComponent<TextMeshProUGUI>().text = JsonUtility.ToJson(settings);
+
+        Transform controlsList = settingsMenu.transform.Find("Controls");
+        for (int i = 0; i < controlsList.childCount; i++) // Will eventually move all control rebinds into their own canvas for simplicity, but this should work fine for now
+        {
+            Transform child = controlsList.GetChild(i);
+            string keyName = GetControl(child.gameObject.name).ToString();
+            if (keyName.Contains("Alpha"))
+            {
+                // Removes "Alpha" from main number keys
+                keyName = keyName.Replace("Alpha", "");
+            }
+            child.Find("Binding").GetComponent<TextMeshProUGUI>().text = keyName;
+        }
     }
 
     public void SaveSettings()
     {
         string settingsToSave = JsonUtility.ToJson(settings, true);
         File.WriteAllText("Settings.data", settingsToSave);
+    }
+
+    public void StartRebind(string controlName)
+    {
+        rebindPrompt.SetActive(true);
+        currentRebind = controlName;
+    }
+
+    public KeyCode GetControl(string controlName)
+    {
+        switch (controlName)
+        {
+            case "moveForward":
+                return settings.keybinds.moveForward;
+            case "moveBackward":
+                return settings.keybinds.moveBackward;
+            case "moveLeft":
+                return settings.keybinds.moveLeft;
+            case "moveRight":
+                return settings.keybinds.moveRight;
+            default:
+                return KeyCode.None;
+        }
+    }
+    
+    public void SetControl(string controlName, KeyCode newKey)
+    {
+        switch (controlName)
+        {
+            case "moveForward":
+                settings.keybinds.moveForward = newKey;
+                break;
+            case "moveBackward":
+                settings.keybinds.moveBackward = newKey;
+                break;
+            case "moveLeft":
+                settings.keybinds.moveLeft = newKey;
+                break;
+            case "moveRight":
+                settings.keybinds.moveRight = newKey;
+                break;
+            
+        }
+    }
+
+    public void ResetAllControls()
+    {
+        //settings.keybinds = Keybinds(false);
     }
 }
 
@@ -49,6 +154,8 @@ public class ClientSettings
     // Could be using the input manager for this, but for now manually setting the key codes should work fine
     [Serializable] public struct Keybinds
     {
+        public KeyCode debugFlash;
+
         public KeyCode moveForward;
         public KeyCode moveBackward;
         public KeyCode moveLeft;
@@ -57,6 +164,8 @@ public class ClientSettings
         public Keybinds(bool settingsLoaded)
         {
             // Will implement settings loaded check later
+            debugFlash = KeyCode.Space;
+
             moveForward = KeyCode.W;
             moveBackward = KeyCode.S;
             moveLeft = KeyCode.A;
