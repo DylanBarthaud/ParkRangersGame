f5bbb0b42a592c27cefb569692f01be0afed5c9d
PinguNSE <148564778+PeppyThePingu@users.noreply.github.com>
2026-02-23 16:00:45 +0000

    Expanded settings menu
    Added menu tabs/submenus, added additional basic settings

--- MultiplayerBase/Assets/_Scripts/SettingsHandler.cs
+++ MultiplayerBase/Assets/_Scripts/SettingsHandler.cs
@@ -5,11 +5,15 @@ using System;
 using TMPro;
 using UnityEngine.InputSystem;
 using System.Collections.Generic;
+using UnityEditor.VersionControl;
+using Unity.VisualScripting.ReorderableList.Element_Adder_Menu;
+using NUnit.Framework;
 
 public class SettingsHandler : MonoBehaviour
 {
     [SerializeField] private GameObject settingsMenu;
     private GameObject rebindPrompt;
+    private GameObject subMenus;
     private string currentRebind;
     private List<KeyCode> forbiddenKeys = new List<KeyCode>
     {
@@ -27,6 +31,7 @@ public class SettingsHandler : MonoBehaviour
     void Start()
     {
         rebindPrompt = settingsMenu.transform.Find("RebindPanel").gameObject;
+        subMenus = settingsMenu.transform.Find("SubMenus").gameObject;
 
         // Attempt to load settings from file, if not found then use default settings
         try
@@ -46,7 +51,7 @@ public class SettingsHandler : MonoBehaviour
         if (currentRebind == null)
         {
             // Temp debug things to make sure things are working, only runs when a rebind prompt isn't active
-            if (Input.GetKey(settings.keybinds.debugFlash))
+            if (Input.GetKey(settings.controls.debugFlash))
             {
                settingsMenu.transform.Find("TempText").gameObject.SetActive(true);
             }
@@ -74,22 +79,70 @@ public class SettingsHandler : MonoBehaviour
         }
     }
 
+    public void OpenSubMenu(GameObject newSubMenu)
+    {
+        foreach (Transform subMenu in subMenus.transform)
+        {
+            subMenu.gameObject.SetActive(false);
+        }
+
+        newSubMenu.SetActive(true);
+    }
+
     public void ReadSettings()
     {
         GameObject tempText = settingsMenu.transform.Find("TempText").gameObject;
         tempText.GetComponent<TextMeshProUGUI>().text = JsonUtility.ToJson(settings);
 
-        Transform controlsList = settingsMenu.transform.Find("Controls");
-        for (int i = 0; i < controlsList.childCount; i++) // Will eventually move all control rebinds into their own canvas for simplicity, but this should work fine for now
+        // Loads accessibility settings
+        Transform accessibilityList = subMenus.transform.Find("Accessibility");
+        for (int i = 0; i < accessibilityList.childCount; i++)
+        {
+            Transform child = accessibilityList.GetChild(i);
+
+            if (child.gameObject.CompareTag("CheckboxSetting"))
+            {
+                child.Find("Checkbox").Find("Checkmark").gameObject.SetActive(GetToggleValue(child.gameObject.name));
+            }
+        }
+
+        // Loads control settings
+        Transform controlsList = subMenus.transform.Find("Controls");
+        for (int i = 0; i < controlsList.childCount; i++)
         {
             Transform child = controlsList.GetChild(i);
-            string keyName = GetControl(child.gameObject.name).ToString();
-            if (keyName.Contains("Alpha"))
+
+            if (child.gameObject.CompareTag("KeybindSetting"))
             {
-                // Removes "Alpha" from main number keys
-                keyName = keyName.Replace("Alpha", "");
+                string keyName = GetControl(child.gameObject.name).ToString();
+                if (keyName.Contains("Alpha"))
+                {
+                    // Removes "Alpha" from main number keys
+                    keyName = keyName.Replace("Alpha", "");
+                }
+                child.Find("Binding").GetComponent<TextMeshProUGUI>().text = keyName;
             }
-            child.Find("Binding").GetComponent<TextMeshProUGUI>().text = keyName;
+            else if (child.gameObject.CompareTag("SliderSetting"))
+            {
+                child.Find("Slider").GetComponent<Slider>().value = GetSliderValue(child.gameObject.name);
+            }
+            
+        }
+
+        // Loads video settings
+        Transform videoList = subMenus.transform.Find("Video");
+
+        // Loads audio settings
+        Transform audioList = subMenus.transform.Find("Audio");
+        for (int i = 0; i < audioList.childCount; i++)
+        {
+            Transform child = audioList.GetChild(i);
+
+            if (child.gameObject.CompareTag("SliderSetting"))
+            {
+                child.Find("Slider").GetComponent<Slider>().value = GetSliderValue(child.gameObject.name);
+            }
+            
         }
     }
 
@@ -110,13 +163,13 @@ public class SettingsHandler : MonoBehaviour
         switch (controlName)
         {
             case "moveForward":
-                return settings.keybinds.moveForward;
+                return settings.controls.moveForward;
             case "moveBackward":
-                return settings.keybinds.moveBackward;
+                return settings.controls.moveBackward;
             case "moveLeft":
-                return settings.keybinds.moveLeft;
+                return settings.controls.moveLeft;
             case "moveRight":
-                return settings.keybinds.moveRight;
+                return settings.controls.moveRight;
             default:
                 return KeyCode.None;
         }
@@ -124,21 +177,117 @@ public class SettingsHandler : MonoBehaviour
     
     public void SetControl(string controlName, KeyCode newKey)
     {
+        
+        string keyName = newKey.ToString();
+
         switch (controlName)
         {
             case "moveForward":
-                settings.keybinds.moveForward = newKey;
+                settings.controls.moveForward = newKey;
+                //bindingText.GetComponent<TextMeshProUGUI>().text = keyName.Replace("Alpha","");
                 break;
             case "moveBackward":
-                settings.keybinds.moveBackward = newKey;
+                settings.controls.moveBackward = newKey;
                 break;
             case "moveLeft":
-                settings.keybinds.moveLeft = newKey;
+                settings.controls.moveLeft = newKey;
                 break;
             case "moveRight":
-                settings.keybinds.moveRight = newKey;
+                settings.controls.moveRight = newKey;
                 break;
             
+        }
+    }
+
+    public bool GetToggleValue(string optionName)
+    {
+        switch (optionName)
+        {
+            // Accessibility checkbox options
+            case "isLeftHanded":
+                return settings.accessibility.isLeftHanded;
+            case "colourblindModeEnabled":
+                return settings.accessibility.colourblindModeEnabled;
+            case "dyslexiaSafeFontEnabled":
+                return settings.accessibility.dyslexiaSafeFontEnabled;
+
+            // Video checkbox options
+            case "isFullscreen":
+                return settings.video.isFullscreen;
+
+            default:
+                return false;
+        }
+    }
+
+    public void SetToggleValue(GameObject option)
+    {
+        string optionName = option.name;
+        GameObject checkbox = option.transform.Find("Checkbox").Find("Checkmark").gameObject;
+
+        switch (optionName)
+        {
+            // Accessibility checkbox options
+            case "isLeftHanded":
+                settings.accessibility.isLeftHanded = !settings.accessibility.isLeftHanded;
+                checkbox.SetActive(settings.accessibility.isLeftHanded);
+                break;
+            case "colourblindModeEnabled":
+                settings.accessibility.colourblindModeEnabled = !settings.accessibility.colourblindModeEnabled;
+                checkbox.SetActive(settings.accessibility.colourblindModeEnabled);
+                break;
+            case "dyslexiaSafeFontEnabled":
+                settings.accessibility.dyslexiaSafeFontEnabled = !settings.accessibility.dyslexiaSafeFontEnabled;
+                checkbox.SetActive(settings.accessibility.dyslexiaSafeFontEnabled);
+                break;
+            
+            // Video checkbox options
+            case "isFullscreen":
+                settings.video.isFullscreen = !settings.video.isFullscreen;
+                checkbox.SetActive(settings.video.isFullscreen);
+                break;
+            
+        }
+    }
+
+    public float GetSliderValue(string settingName)
+    {
+        switch (settingName)
+        {
+            // Video slider settings
+            case "camSensitivity":
+                return settings.controls.camSensitivity;
+
+            // Audio slider settings
+            case "musicVolume":
+                return settings.audio.musicVolume;
+            case "sfxVolume":
+                return settings.audio.sfxVolume;
+
+            default:
+                return 0.5f;
+        }
+    }
+
+    public void SetSliderValue(GameObject setting)
+    {
+        string settingName = setting.name;
+        GameObject slider = setting.transform.Find("Slider").gameObject;
+
+        switch (settingName)
+        {
+            // Video slider settings
+            case "camSensitivty":
+                settings.controls.camSensitivity = slider.GetComponent<Slider>().value;
+                break;
+
+            // Audio slider settings
+            case "musicVolume":
+                settings.audio.musicVolume = slider.GetComponent<Slider>().value;
+                break;
+            case "sfxVolume":
+                settings.audio.sfxVolume = slider.GetComponent<Slider>().value;
+                break;
         }
     }
 
@@ -151,21 +300,38 @@ public class SettingsHandler : MonoBehaviour
 [Serializable]
 public class ClientSettings
 {
-    // Could be using the input manager for this, but for now manually setting the key codes should work fine
-    [Serializable] public struct Keybinds
+    [Serializable] public struct AccessibilityOptions
     {
+        public bool isLeftHanded;
+        public bool colourblindModeEnabled;
+        public bool dyslexiaSafeFontEnabled;
+
+        public AccessibilityOptions(bool settingsLoaded)
+        {
+            isLeftHanded = false;
+            colourblindModeEnabled = false;
+            dyslexiaSafeFontEnabled = false;
+        }
+    }
+
+    // Could be using the input manager for this, but for now manually setting the key codes should work fine
+    [Serializable] public struct Controls
+    {
+
         public KeyCode debugFlash;
 
+        public float camSensitivity;
         public KeyCode moveForward;
         public KeyCode moveBackward;
         public KeyCode moveLeft;
         public KeyCode moveRight;
 
-        public Keybinds(bool settingsLoaded)
+        public Controls(bool settingsLoaded)
         {
             // Will implement settings loaded check later
             debugFlash = KeyCode.Space;
 
+            camSensitivity = 0.5f;
             moveForward = KeyCode.W;
             moveBackward = KeyCode.S;
             moveLeft = KeyCode.A;
@@ -175,12 +341,14 @@ public class ClientSettings
 
     [Serializable] public struct Audio
     {
-        public float volume;
+        public float musicVolume;
+        public float sfxVolume;
 
         public Audio(bool settingsLoaded)
         {
             // Will implement settings loaded check later
-            volume = 0.5f;
+            musicVolume = 0.5f;
+            sfxVolume = 0.5f;
         }
     }
 
@@ -199,14 +367,16 @@ public class ClientSettings
         }
     }
 
-    public Keybinds keybinds;
-    public Audio audioSettings;
-    public Video videoSettings;
+    public AccessibilityOptions accessibility;
+    public Controls controls;
+    public Audio audio;
+    public Video video;
 
     public ClientSettings(bool settingsLoaded)
     {
-        keybinds = new Keybinds(settingsLoaded);
-        audioSettings = new Audio(settingsLoaded);
-        videoSettings = new Video(settingsLoaded);
+        accessibility = new AccessibilityOptions(settingsLoaded);
+        controls = new Controls(settingsLoaded);
+        audio = new Audio(settingsLoaded);
+        video = new Video(settingsLoaded);
     }
 }
