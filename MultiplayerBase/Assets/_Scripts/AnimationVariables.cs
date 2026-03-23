7bbb38814edb3d0653fef77509138edc0c8ac175
Glitch-o <budderking123.1@gmail.com>
2026-03-23 17:14:41 +0000

    walking + running animations working now kind of

---
+++ MultiplayerBase/Assets/_Scripts/AnimationVariables.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AnimationVariables : MonoBehaviour
+{
+
+    NavMeshAgent _agent;
+    Animator _animator;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        _animator = GetComponentInChildren<Animator>();
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            _animator.SetBool("isWalking", true);
+            Debug.Log("WALKING");
+        }
+        else
+        {
+            _animator.SetBool("isWalking", false);
+            
+        }
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            _animator.SetBool("isRunning", true);
+            Debug.Log("RUNNING");
+        }
+        else
+        {
+            _animator.SetBool("isRunning", false);
+        }
+        
+        if (Input.GetKey(KeyCode.Space))
+        {
+            _animator.SetBool("isJump", true);
+        }
+        else
+        {
+            _animator.SetBool("isJump", false);
+        }
+    }
+}
