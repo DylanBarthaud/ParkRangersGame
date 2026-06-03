113de1d064213aa1f4adf9966901ac6b75604c4a
DylanThyGamer <piemaster5556@gmail.com>
2026-06-03 15:23:02 +0100

    CheckIn UI

---
+++ MultiplayerBase/Assets/CheckInManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class CheckInManager : MonoBehaviour
+{
+    [SerializeField] private GameObject uiPanel;
+    private Interactor interactor;
+    private CheckInTable currentTable; 
+
+    public void OpenMenu() => uiPanel.SetActive(true);
+    public void CloseMenu()
+    {
+        FirstPersonController playerController = interactor.GetComponent<FirstPersonController>();
+        if (playerController != null)
+        {
+            playerController.EnableMovement();
+            interactor.GetComponent<Inventory>().EnableInv();
+        }
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+
+        uiPanel.SetActive(false);
+    }
+
+    public void SetInteractor(Interactor interactor) => this.interactor = interactor;
+    public void SetTable(CheckInTable table) => this.currentTable = table;
+    public void CheckIn() => interactor.SetZone(currentTable.Zone);
+    public void CompleteZone() => currentTable.CompleteZone();
+}
--- MultiplayerBase/Assets/CheckInTable.cs
+++ MultiplayerBase/Assets/CheckInTable.cs
@@ -1,14 +1,49 @@
+using System;
 using UnityEngine;
 
 public class CheckInTable : MonoBehaviour, IInteractable
 {
-    [SerializeField] private Zones zone; 
+    [SerializeField] private Zones zone;
+    public Zones Zone => zone;
+    [SerializeField] private int tasksInZone = 5;
+    [SerializeField] private CheckInManager checkInManager;
+    private int tasksComplete = 0; 
+    private bool zoneComplete = false;
+    Interactor interactor;
+
+    private void Awake()
+    {
+        EventManager.instance.onPuzzleComplete += OnPuzzleComplete;
+    }
 
     public void OnInteract(Interactor interactor, ItemType itemUsed = ItemType.None)
     {
-        interactor.SetZone(zone);
-        Debug.Log($"set to zone {zone}"); 
+        FirstPersonController playerController = interactor.GetComponent<FirstPersonController>();
+        if (playerController != null)
+        {
+            playerController.DisableMovement();
+            interactor.GetComponent<Inventory>().DisableInv();
+        }
+
+        Cursor.lockState = CursorLockMode.Confined;
+        Cursor.visible = true;
+
+        checkInManager.SetInteractor(interactor);
+        checkInManager.SetTable(this); 
+        checkInManager.OpenMenu(); 
     }
 
     public bool RequiresZoneCheckIn() { return false; }
+
+    private void OnPuzzleComplete(bool sucess, IInteractable interactable)
+    {
+        if (!sucess) return;
+        if (GameManager.instance.PlayerInSameZone(zone)) tasksComplete++;
+        Debug.Log(tasksComplete + "Tasks complete in: " + zone); 
+    }
+
+    public void CompleteZone()
+    {
+        if(tasksComplete >= tasksInZone) zoneComplete = true;
+    }
 }
