e3f576c480ba88950ac3eb64f496af9c907ee83c
Glitch-o <budderking123.1@gmail.com>
2026-06-11 16:36:00 +0100

    no longer posting an incomprehensible amount of errors

--- MultiplayerBase/Assets/_Scripts/EndTrigger.cs
+++ MultiplayerBase/Assets/_Scripts/EndTrigger.cs
@@ -2,13 +2,14 @@ using UnityEngine;
 
 public class EndTrigger : MonoBehaviour
 {
-    private int playersIn;
-    private int amountOfPlayers;
+    private int playersIn = 0;
+    private int amountOfPlayers = 0;
     private GameManager _gameManager;
 
     void Awake()
     {
         _gameManager = gameObject.GetComponent<GameManager>();
+        amountOfPlayers = _gameManager.numberOfPlayers;
     }
 
     void OnTriggerEnter()
@@ -25,11 +26,16 @@ public class EndTrigger : MonoBehaviour
 
     void Update()
     {
+        Debug.Log(amountOfPlayers);
+
+        _gameManager = gameObject.GetComponent<GameManager>();
         _gameManager.numberOfPlayers = amountOfPlayers;
-        if (amountOfPlayers == playersIn)
+
+        if (playersIn == amountOfPlayers*2 && playersIn >= 1)
         {
             Debug.Log("YOU WIN!");
             _gameManager.EndGame();
         }
+        
     }
 }
