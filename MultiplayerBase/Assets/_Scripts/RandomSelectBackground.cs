50f442d2b555efff59dccab2d6a202dc435634cc
Thomas-Cannard <131602734+Thomas-Cannard@users.noreply.github.com>
2026-07-04 03:20:40 +0100

    Added new loading screen backgrounds, changed torch positions, Added torch additionlight, added cursor and new game icon

---
+++ MultiplayerBase/Assets/_Scripts/RandomSelectBackground.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RandomSelectBackground : MonoBehaviour
+{
+
+    public Image TargetImage; 
+    public List<Sprite> bgImage = new List<Sprite>();
+
+    void Start()
+    {
+        Sprite randomBG = bgImage[UnityEngine.Random.Range(0, bgImage.Count)];
+
+        TargetImage.sprite = randomBG;
+    }
+
+    
+}
