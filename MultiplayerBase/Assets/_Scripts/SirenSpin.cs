eaa3fac1ff51c6441bc83e1450e36407b187f4c3
Thomas-Cannard <131602734+Thomas-Cannard@users.noreply.github.com>
2026-07-07 14:41:40 +0100

    Updated Main Menu

--- MultiplayerBase/Assets/_Scripts/SirenSpin.cs
+++ MultiplayerBase/Assets/_Scripts/SirenSpin.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class SirenSpin : MonoBehaviour
@@ -6,12 +7,26 @@ public class SirenSpin : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        StartCoroutine(Spin());
     }
 
     // Update is called once per frame
     void Update()
     {
-        gameObject.transform.Rotate(0f, speed, 0f, Space.World);
+        //gameObject.transform.Rotate(0f, speed, 0f, Space.World);
     }
+
+
+    IEnumerator Spin()
+    {
+        float i = 0;
+        while (i < 1)
+        {
+            gameObject.transform.Rotate(0f, speed, 0f, Space.World);
+            yield return null;
+        }
+
+        yield return new WaitForSeconds(.1f);
+    }
+
 }
