d747ccb9e25f6c33b09f7fc10dc8980cb0fb8e50
Thomas-Cannard <131602734+Thomas-Cannard@users.noreply.github.com>
2026-07-14 17:55:48 +0100

    Footsteps work

--- MultiplayerBase/Assets/FootstepsPlayerScript.cs
+++ MultiplayerBase/Assets/FootstepsPlayerScript.cs
@@ -11,6 +11,7 @@ public class FootstepsPlayerScript : MonoBehaviour
     public AudioClip jumpSound;
     public AudioClip landSound;
     public AudioSource audioSource;
+    private bool eventTrigger = true;
     
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -35,18 +36,50 @@ public class FootstepsPlayerScript : MonoBehaviour
     {
         while (true)
             {
-                yield return new WaitForSeconds(.5f);
-            Debug.Log(playerRef.rb.linearVelocity.magnitude);
-                if (playerRef.IsGrounded && playerRef.rb.linearVelocity.magnitude > 1)
+                yield return new WaitForSeconds(.01f);
+            //Debug.Log(playerRef.joint.localPosition.magnitude);
+            /*
+            if (playerRef.IsGrounded && playerRef.rb.linearVelocity.magnitude > 1)
+            {
+                PlayFootstepSound();
+
+            }
+            else
+            {
+                yield return null;
+            }
+            */
+            if (eventTrigger == true)
+            {
+
+                if (playerRef.IsGrounded && playerRef.rb.linearVelocity.magnitude > 1 && playerRef.joint.localPosition.magnitude > 0.8f)
                 {
+                    eventTrigger = false;
                     PlayFootstepSound();
+                    Debug.Log("played footstep sound");
+
+                }
+                else if (playerRef.joint.localPosition.magnitude < 0.76f)
+                {
+                    //eventTrigger = true;
+                    //Debug.Log("ResetSound");
 
                 }
                 else
                 {
-                    yield return null;
+                    //yield return null;
                 }
             }
+            else if (playerRef.joint.localPosition.magnitude < 0.76f)
+            {
+                eventTrigger = true;
+                Debug.Log("ResetSound");
+            }
+            else
+            {
+                yield return null;
+            }
+        }
         
     }
 }
